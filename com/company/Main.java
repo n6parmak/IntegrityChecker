7e2ed08280848d47ae3be302eb30b22c25816331
n6parmak <6parmaknur@gmail.com>
2020-12-06 11:38:56 +0300

    keytool keypair implementation added.

--- com/company/Main.java
+++ com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.concurrent.Executors;
 
 public class Main {
@@ -13,14 +15,28 @@ public class Main {
         if (isWindows) {
             builder.command("cmd.exe", "/c", "keytool -genkey -alias mydomain -keyalg RSA -keystore E:\\ideaprojects\\untitled8\\a\\keystore.jks -keysize 2048");
         } else {
-            builder.command("sh", "-c", "ls");
+            builder.command("sh", "-c", "keytool -genkey -alias mydomain -keyalg RSA -keystore E:\\ideaprojects\\untitled8\\a\\keystore.jks -keysize 2048");
         }
         builder.directory(new File(System.getProperty("user.home")));
         Process process = builder.start();
+        final PrintWriter writer = new PrintWriter(
+                new OutputStreamWriter(process.getOutputStream())
+        );
+        writer.print("123456\n"); // These and other writer.print(...) statements
+        writer.print("123456\n"); // write answers to questions of keytool
+        writer.print("software developers\n");
+        writer.print("San Francisco\n");
+        writer.print("California\n");
+        writer.print("US\n");
+        writer.print("yes\n");
+        writer.print("yes\n");
+        writer.print("yes\n");
+        writer.print("123456\n"); // These and other writer.print(...) statements
+        writer.print("123456\n");
+        writer.close();
         StreamGobbler streamGobbler =
                 new StreamGobbler(process.getInputStream(), System.out::println);
         Executors.newSingleThreadExecutor().submit(streamGobbler);
-        int exitCode = process.waitFor();
-        assert exitCode == 0;
+
     }
 }
