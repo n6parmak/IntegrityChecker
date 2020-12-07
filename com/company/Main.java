9dfaa42af2d2dd092eb492eb140a878716bad7d2
Nur <61030013+n6parmak@users.noreply.github.com>
2020-12-07 21:06:16 +0300

    Update Main.java

--- com/company/Main.java
+++ com/company/Main.java
@@ -1,136 +1,112 @@
 package com.company;
+import com.company.StreamGobbler;
 
-import jdk.nashorn.internal.ir.LiteralNode;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
 import java.io.*;
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.security.*;
-import java.util.Base64;
-import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.concurrent.Executors;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, InvalidAlgorithmParameterException, NoSuchPaddingException {
-	// write your code here
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        String privateKey = read_private_file();
-        privateKey = privateKey + "This is the private key file";
-        System.out.println(privateKey);
-        String password = getPass();
-        System.out.println("This is the password " + password);
-        String hashedPass = getMd5(password);
-        System.out.println("This is the hashed password " +hashedPass);
-        String encrypted = encrypt(privateKey, hashedPass);
-        System.out.println("This is the encrypted text " +encrypted);
-        System.out.println("This is the decrypted text " + decrypt(encrypted, hashedPass));
-
-        File folder = new File("E:\\ideaprojects\\untitled9\\deneme-directory");
-        File[] listOfFiles = folder.listFiles();
-        String hashtype ="sha-256";
-        assert listOfFiles != null;
-        for (File file : listOfFiles) {
-            if (file.isFile()) {
-                if (hashtype.equals("md5")){
-                    getMd5byte(file.getPath());
-                }
-                else{
-                    getsha256byte(file.getPath());
-
-                }
-            }
+    public static void main(String[] args) throws IOException, InterruptedException, GeneralSecurityException {
+        boolean isWindows = System.getProperty("os.name")
+                .toLowerCase().startsWith("windows");
+        ProcessBuilder builder = new ProcessBuilder();
+        if (isWindows) {
+            builder.command("cmd.exe", "/c", "keytool -genkey -keyalg RSA -keystore E:\\ideaprojects\\untitled8\\a\\keystore.jks -keysize 2048");
+        } else {
+            builder.command("sh", "-c", "keytool -genkey -keyalg RSA -keystore /home/seyma/IdeaProjects/project_3/keystore.jks -keysize 2048");
         }
+        builder.directory(new File(System.getProperty("user.home")));
+        Process process = builder.start();
+        final PrintWriter writer = new PrintWriter(
+                new OutputStreamWriter(process.getOutputStream())
+        );
+        writer.print("123456\n"); // These and other writer.print(...) statements
+        writer.print("123456\n"); // write answers to questions of keytool
+        writer.print("software developers\n");
+        writer.print("San Francisco\n");
+        writer.print("California\n");
+        writer.print("US\n");
+        writer.print("yes\n");
+        writer.print("yes\n");
+        writer.print("yes\n");
+        writer.print("123456\n"); // These and other writer.print(...) statements
+        writer.print("123456\n");
+        writer.close();
+        StreamGobbler streamGobbler =
+                new StreamGobbler(process.getInputStream(), System.out::println);
+        Executors.newSingleThreadExecutor().submit(streamGobbler);
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        Thread.sleep(10000);
+
+        ProcessBuilder builder2 = new ProcessBuilder();
+        if (isWindows) {
+            builder2.command("cmd.exe", "/c", "keytool -export -keystore E:\\ideaprojects\\untitled8\\a\\keystore.jks -rfc -file E:\\ideaprojects\\untitled8\\a\\public.crt");
+
+        } else {
+            builder2.command("sh", "-c", "keytool -export -keystore keystore.jks -rfc -file /home/seyma/IdeaProjects/project_3/public.crt");
+        }
+        builder.directory(new File(System.getProperty("user.home")));
+        Process process2 = builder2.start();
+        final PrintWriter writer2 = new PrintWriter(
+                new OutputStreamWriter(process2.getOutputStream())
+        );
+        writer2.print("123456\n"); // These and other writer.print(...) statements
+        writer2.print("123456\n");
+        writer2.close();
+        StreamGobbler streamGobbler2 =
+                new StreamGobbler(process2.getInputStream(), System.out::println);
+        Executors.newSingleThreadExecutor().submit(streamGobbler2);
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        Thread.sleep(2000);
+
+        ProcessBuilder builder3 = new ProcessBuilder();
+        if (isWindows) {
+            builder3.command("cmd.exe", "/c", "keytool -importkeystore -srckeystore E:\\ideaprojects\\untitled8\\a\\keystore.jks -srcstorepass 123456 -destkeystore E:\\ideaprojects\\untitled8\\a\\identity.p12 -deststoretype PKCS12 -deststorepass 123456 -destkeypass 123456");
+
+        } else {
+
+            builder3.command("sh", "-c", "keytool -importkeystore -srckeystore /home/seyma/IdeaProjects/project_3/keystore.jks -srcstorepass 123456 -destkeystore identity.p12 -deststoretype PKCS12 -deststorepass 123456 -destkeypass 123456");
+        }
+        builder.directory(new File(System.getProperty("user.home")));
+        Process process3 = builder3.start();
+        StreamGobbler streamGobbler3 =
+                new StreamGobbler(process3.getInputStream(), System.out::println);
+        Executors.newSingleThreadExecutor().submit(streamGobbler3);
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        Thread.sleep(10000);
+
+        ProcessBuilder builder4 = new ProcessBuilder();
+        if (isWindows) {
+            builder4.command("cmd.exe", "/c","openssl pkcs12 -in E:\\ideaprojects\\untitled8\\a\\identity.p12 -nodes -nocerts -out E:\\ideaprojects\\untitled8\\a\\private_key.pem");
+
+        } else {
+            builder4.command("sh", "-c", "openssl pkcs12 -in identity.p12 -nodes -nocerts -out private_key.pem");
+        }
+        builder.directory(new File(System.getProperty("user.home")));
+        Process process4= builder4.start();
+        final PrintWriter writer4 = new PrintWriter(
+                new OutputStreamWriter(process4.getOutputStream())
+        );
+        writer4.print("123456\n"); // These and other writer.print(...) statements
+        writer4.print("123456\n");
+        writer4.close();
+        StreamGobbler streamGobbler4 =
+                new StreamGobbler(process4.getInputStream(), System.out::println);
+        Executors.newSingleThreadExecutor().submit(streamGobbler4);
+        Thread.sleep(100000);
+        read_private_file();
+
     }
 
-    private static void getsha256byte(String filepath) {
-        try {
-            byte[] b = Files.readAllBytes(Paths.get(filepath));
-            byte[] hash = MessageDigest.getInstance("SHA-256").digest(b);
-            String actual = DatatypeConverter.printHexBinary(hash);
-            System.out.println(filepath+" adlı dosyanın sha-256 hashı "+actual);
-        }
-
-        catch (NoSuchAlgorithmException e) {
-
-            System.out.println("Exception thrown : " + e);
-        }
-        catch (NullPointerException e) {
-
-            System.out.println("Exception thrown : " + e);
-        }
-        catch (FileNotFoundException e) {
-
-            System.out.println("Exception thrown : " + e);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static String getMd5(String input) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-
-            byte[] messageDigest = md.digest(input.getBytes());
-
-            BigInteger no = new BigInteger(1, messageDigest);
-
-            String hashtext = no.toString(16);
-            while (hashtext.length() < 32) {
-                hashtext = "0" + hashtext;
-            }
-            return hashtext;
-        }
-
-        catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    public static void getMd5byte(String filepath) {
-        try {
-            byte[] b = Files.readAllBytes(Paths.get(filepath));
-            byte[] hash = MessageDigest.getInstance("MD5").digest(b);
-            String actual = DatatypeConverter.printHexBinary(hash);
-            System.out.println(filepath+" adlı dosyanın md5 hashı "+actual);
-        }
-
-        catch (NoSuchAlgorithmException e) {
-
-            System.out.println("Exception thrown : " + e);
-        }
-        catch (NullPointerException e) {
-
-            System.out.println("Exception thrown : " + e);
-        }
-        catch (FileNotFoundException e) {
-
-            System.out.println("Exception thrown : " + e);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static String getPass() {
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter username");
-
-        String password = myObj.nextLine();  // Read user input
-        System.out.println("Username is: " + password);  // Output user input
-        return password;
-    }
-
-    private static String read_private_file() throws IOException {
-        File f = new File("E:\\ideaprojects\\untitled9\\src\\com\\company\\private_key.pem");
+    private static void read_private_file() throws IOException {
+        File f = new File("E:\\ideaprojects\\untitled8\\a\\private_key.pem");
         FileInputStream fis = new FileInputStream(f);
         DataInputStream dis = new DataInputStream(fis);
         byte[] keyBytes = new byte[(int) f.length()];
@@ -141,39 +117,8 @@ public class Main {
         int a = key.indexOf("-");
         key = key.substring(a+"-----BEGIN PRIVATE KEY-----\n".length());
         key = key.replace("-----END PRIVATE KEY-----","").trim();
-        return key;
+        System.out.println(key);
+        System.out.println(key);
     }
 
-    public static String encrypt(String input, String key) {
-        byte[] crypted = null;
-        try {
-
-            SecretKeySpec skey = new SecretKeySpec(key.getBytes(), "AES");
-
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, skey);
-            crypted = cipher.doFinal(input.getBytes());
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        java.util.Base64.Encoder encoder = java.util.Base64.getEncoder();
-
-        return new String(encoder.encodeToString(crypted));
-    }
-
-    public static String decrypt(String input, String key) {
-        byte[] output = null;
-        try {
-            java.util.Base64.Decoder decoder = java.util.Base64.getDecoder();
-            SecretKeySpec skey = new SecretKeySpec(key.getBytes(), "AES");
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.DECRYPT_MODE, skey);
-            output = cipher.doFinal(decoder.decode(input));
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        return new String(output);
-    }
-
-    }
-
+}
